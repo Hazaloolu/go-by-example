710803acfb2dda063b5ba22ec79e8d66170e9c4f
Adetayo <adetunjitayo2019@gmail.com>
2024-08-13 03:25:33 +0100

    closures

--- practice.go
+++ practice.go
@@ -4,14 +4,24 @@ import (
 	"fmt"
 )
 
-func sum(nums ...int) int {
-	total := 0
+// func sum(nums ...int) int {
+// 	total := 0
 
-	for _, num := range nums {
-		total += num
+// 	for _, num := range nums {
+// 		total += num
 
+// 	}
+// 	return total
+// }
+
+// closures
+
+func counter() func() int {
+	count := 0
+	return func() int {
+		count++
+		return count
 	}
-	return total
 }
 
 func main() {
@@ -37,12 +47,20 @@ func main() {
 	// 	fmt.Println(("It is a weekday"))
 	// }
 
-	// variadic function
+	// VARIADIC FUNCTION
 
-	result1 := sum(1, 2, 3)
-	result2 := sum(1, 2, 3, 4, 5)
+	// result1 := sum(1, 2, 3)
+	// result2 := sum(1, 2, 3, 4, 5)
 
-	fmt.Println(result1)
-	fmt.Println(result2)
+	// fmt.Println(result1)
+	// fmt.Println(result2)
+
+	// CLOSURES
+
+	increment := counter()
+
+	fmt.Println(increment())
+	fmt.Println(increment())
+	fmt.Println(increment())
 
 }
